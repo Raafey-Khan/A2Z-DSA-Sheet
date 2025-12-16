708336a63689f25711eef1da487ecf38cba53a25
Raafey-Khan <loyaalraafay@gmail.com>
2025-12-17 00:47:29 +0530

    added data types

---
+++ DATATYPES.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // int, long, long long, float, double
+    // string and getline
+    // chars
+
+    char ch = 'g';
+    cin >> ch;
+    cout << "THe output" << ch << endl;
+
+    return 0;
+}
+
+
+// int = integer
+
+// long = longer number at least 32 bits 32 bit on windows and 64 bit on linux macos
+
+// long long int : always at leaset 64 bits
+
+//  string and getline
+
+// getling(cin <-- takes the input of string, str <-- variable)
+
+// string and getline used to take any string as a input
+
+// there are 250 char in english dictionary
+
+// if you define string in cpp then string takes in double quotes
+// well as char can be written in single quotes also
+
+
+/*
+
+These are the basic types provided by C++.
+Integer types
+int
+short
+long
+long long
+Unsigned integer types
+unsigned int
+unsigned short
+unsigned long
+unsigned long long
+Character types
+char
+signed char
+unsigned char
+wchar_t
+char16_t
+char32_t
+Floating-point types
+float
+double
+long double
+Boolean type
+bool
+Special type
+void
+*/
